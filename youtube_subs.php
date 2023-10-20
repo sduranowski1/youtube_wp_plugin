6d2345dcace528474f3ff772d408121af9838a59
Simon Duranowski <sduranowski1@gmail.com>
2023-10-20 22:35:47 +0200

    first commit

---
+++ youtube_subs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: YouTube Subs
+Plugin URI: https://roncee.tech/
+Description: Checks the health of your WordPress install
+Version: 1.0.0
+Author: Szymon Duranowski
+Author URI: https://roncee.tech/
+*/
